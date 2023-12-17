ece19c602abea22b9905573fa6bbb8ef69a26859
kkk <73045939+KuangSheng34@users.noreply.github.com>
2023-12-17 23:17:13 +0800

    Delete OOADbackend directory

--- OOADbackend/spring-project-main/src/main/java/com/example/springproject/repository/TeacherRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.springproject.repository;
-
-import com.example.springproject.domain.TeacherInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeacherRepository extends JpaRepository<TeacherInfo,String> {
-
-}
